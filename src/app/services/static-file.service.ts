f1e5f5fbcf066bf079cce5ee2077ff51fa0e3f3b
Mariusz Luciow <mariusz.luciow@ocado.com>
2018-12-14 12:21:35 +0100

    Closes https://github.com/ocadotechnology/ganttilla/issues/2

---
+++ src/app/services/static-file.service.ts
@@ -0,0 +1,24 @@
+import {Injectable} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {Observable} from 'rxjs';
+
+export class Goal {
+  title: string;
+  category: string;
+  start_date: string;
+  end_date: string;
+  description: string;
+  link: string;
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class StaticFileService {
+
+  constructor(private http: HttpClient) { }
+
+  getFile(filename: string): Observable<Goal[]> {
+    return this.http.get<Goal[]>(`./assets/${filename}`);
+  }
+}
