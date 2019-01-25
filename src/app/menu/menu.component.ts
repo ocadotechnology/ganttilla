744b08da33325b41fed2fe97775468237862ac14
Lukasz Kaszczyszyn <l.kaszczyszyn@ocado.com>
2019-01-25 15:07:47 +0100

    configurable menu

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -1,10 +1,22 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {StaticFileService} from '../services/static-file.service';
 
 @Component({
     selector: 'app-menu',
     templateUrl: './menu.component.html',
     styleUrls: ['./menu.component.css']
 })
-export class MenuComponent {
+export class MenuComponent implements OnInit {
+    menu = [];
+
+    constructor(
+        private fileService: StaticFileService
+    ) {}
+
+    ngOnInit() {
+        this.fileService.getFile('menu.json').subscribe(menu => {
+            this.menu = menu;
+        });
+    }
 
 }
